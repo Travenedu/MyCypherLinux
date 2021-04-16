c4604e9ae6a13618331332904b90b0ab516f7f8b
Traven Reese <travenreese3@gmail.com>
2021-04-16 19:42:42 +0000

    second check in

--- mycypher.py
+++ mycypher.py
@@ -1,2 +1,32 @@
 t = input("How many shifts\n")
 s = int(t)
+text = input("What is your message?\n")
+def encrypt(text,s):
+    str = ""
+    desi = []
+    final = []
+    CAP = text.upper()
+    for i in CAP:
+        desi.append(i)
+    for i in desi:
+        if i == " " or i == ' ' or i == "`" or i == "~" or i == "!" or i == "@" or i == "#" or i == "$" or i == "%":
+            desi.remove(i)
+        if i == "^" or i == "&" or i == "*" or i == "(" or i == ")" or i == "_" or i == "-" or i == "+":
+            desi.remove(i)
+        if i == "=" or i == "{" or i == "}" or i == "[" or i == "]" or i == "|" or i == "" or i == ":":
+            desi.remove(i)
+        if i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8":
+            desi.remove(i)
+        if i == "9" or i == "0" or i == ";" or i == "<" or i == ">" or i == "?" or i == "/":
+            desi.remove(i)
+        if i == "," or i == ".":
+            desi.remove(i)
+    for i in desi:
+        if i == " ":
+            desi.remove(i)
+    result = ""
+    for i in range(len(desi)):
+        character = desi[i]
+        result += chr((ord(character) + s-65) % 26 + 65)
+    for i in result:
+        final.append(i)
